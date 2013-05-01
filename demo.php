152e166e22ed90f596c19c34519b3f4e28623bec
Gautam Rajput <gtrj55@gmail.com>
2013-05-01 17:16:46 +0530

    dekh lo
    thik hai

--- demo.php
+++ demo.php
@@ -1,3 +1,8 @@
 <?php
-echo "hello";
+$connection=mysql_connect("localhost","root","");
+mysql_select_db("plsql",$connection);
+$query=mysql_query("insert into test(name,photo,email,mobile,address) values("gautam","rajput","gtrj55",98260,"narela");
+echo $query;
+mysql_close($connection);
+
 ?>
